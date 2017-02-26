956564b5965274d88da5914a1a0458dddb8577e1
Garen Liang <ideabreeze@gmail.com>
2017-02-26 15:58:20 -0800

    Create 18-QueueLinkedlist.js

---
+++ 18-QueueLinkedlist.js
@@ -0,0 +1,49 @@
+/*
+Algorithm
+
+We will store a reference to the front and back of the queue in order to make enqueuing and dequeuing run in O(1) 
+constant time. Every time we want to insert into the queue, we add the new element to the end of the linked list 
+and update the back pointer. When we want to dequeue we return the first node in the linked list and update the front pointer.
+
+
+*/
+
+// queue is initially empty
+var Queue = {front: null, back: null};
+
+// we will use a node to keep track of the elements
+// in the queue which is represented by a linked list
+function Node(data, next) {
+  this.data = data;
+  this.next = next;
+} 
+
+// add elements to queue in O(1) time
+function Enqueue(element) {
+  var N = new Node(element, null);
+  if (Queue.back === null) {
+    Queue.front = N;
+    Queue.back = N; 
+  }
+  else { 
+    Queue.back.next = N; 
+    Queue.back = Queue.back.next;
+  } 
+}
+
+// remove first element from queue in O(1) time
+function Dequeue() {
+  if (Queue.front !== null) { 
+    var first = Queue.front;
+    Queue.front = Queue.front.next; 
+    return first.data;
+  } else {
+    if (Queue.back !== null) { Queue.back = null; }
+    return 'Cannot dequeue because queue is empty';
+  }
+}
+
+Enqueue('a'); 
+Enqueue('b'); 
+Enqueue('c'); 
+Dequeue();
