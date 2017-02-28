a48ec7436147bd844fc061270480312ce926f01d
Garen Liang <ideabreeze@gmail.com>
2017-02-27 19:58:32 -0800

    Update and rename 34- to 34-MiddleNode.js

--- 34-MiddleNode.js
+++ 34-MiddleNode.js
@@ -34,6 +34,11 @@ Faster solution using 2 pointers
 What we'll do is setup two pointers, one that will traverse the linked list one node at a time, and the other pointer will traverse two nodes at a time. This way when the faster pointer reaches the end of the linked list, the slower pointer will be halfway there because it was only moving one node at time while the faster one was moving two nodes at a time. This allows you to find the middle node of a linked list with only one pass, instead of passing through the whole linked list once, 
 and then again to find the middle element.
 
+Running time
+
+The running time of finding the middle element this way with two pointers is O(n) because once we pass through the entire linked list of n elements, the slower pointer is at the middle node already.
+
+
 */
 
 
